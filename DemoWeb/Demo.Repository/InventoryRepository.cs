c626e088c831c5ad68e72b4e882b8c3829544d1a
horace.yeh <horaceyeh.web@gmail.com>
2020-07-29 20:22:22 +0800

    1.新增 Inventory、Material repository CRUD

---
+++ DemoWeb/Demo.Repository/InventoryRepository.cs
@@ -0,0 +1,107 @@
+﻿using Demo.Data.Models;
+using Demo.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UtilHelper;
+
+namespace Demo.Repository
+{
+    public class InventoryRepository : IInventoryRepository
+    {
+        private DapperTool dapperTool;
+
+        public InventoryRepository()
+        {
+            this.dapperTool = new DapperTool();
+        }
+
+        public void Create(Inventory instance)
+        {
+            var sql = @"INSERT INTO [dbo].[Inventory]
+                         ([MaterialID]
+                         ,[Quantity]
+                         ,[Creater]
+                         ,[LastEditor])
+                          VALUES
+                         (@MaterialID
+                         ,@Quantity
+                         ,@Creater
+                         ,@LastEditor)";
+            var pms = new
+            {
+                MaterialID = instance.MaterialID,
+                Quantity = instance.Quantity,
+                Creater = instance.Creater,
+                LastEditor = instance.LastEditor
+            };
+            var data = this.dapperTool.DapperNonQuery(sql, pms);
+        }
+
+        public void Delete(Inventory instance)
+        {
+            var sql = @"DELETE FROM [dbo].[Inventory]
+                        WHERE [ID] = @ID";
+            var pms = new 
+            {
+                ID = instance.ID
+            };
+            var data = this.dapperTool.DapperNonQuery(sql, pms);
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Inventory Get(int primaryID)
+        {
+            var sql = @"SELECT [ID]
+                         ,[MaterialID]
+                         ,[Quantity]
+                         ,[Creater]
+                         ,[CreatDate]
+                         ,[LastEditor]
+                         ,[LastUpdate]
+                     FROM [dbo].[Inventory]
+                     WHERE [ID] = @ID";
+            var pms = new { ID = primaryID };
+            var data = this.dapperTool.DapperQuery<Inventory>(sql, pms).FirstOrDefault();
+            return data;
+        }
+
+        public IList<Inventory> GetAll()
+        {
+            var sql = @"SELECT [ID]
+                        ,[MaterialID]
+                        ,[Quantity]
+                        ,[Creater]
+                        ,[CreatDate]
+                        ,[LastEditor]
+                        ,[LastUpdate]
+                    FROM [dbo].[Inventory]";
+            var data = this.dapperTool.DapperQuery<Inventory>(sql, null);
+            return data;
+        }
+
+        public void Update(Inventory instance)
+        {
+            var sql = @"UPDATE [dbo].[Inventory]
+                          SET [MaterialID] = @MaterialID
+                             ,[Quantity] = @Quantity
+                             ,[LastEditor] = @LastEditor
+                             ,[LastUpdate] = GETDATE()
+                        WHERE [ID] = @ID";
+            var pms = new 
+            {
+                ID = instance.ID,
+                MaterialID = instance.MaterialID,
+                Quantity = instance.Quantity,
+                LastEditor = instance.LastEditor
+            };
+            var data = this.dapperTool.DapperNonQuery(sql, pms);
+        }
+    }
+}
---
+++ DemoWeb/Demo.Repository/MaterialRepository.cs
@@ -0,0 +1,104 @@
+﻿using Demo.Data.Models;
+using Demo.Repository.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UtilHelper;
+
+namespace Demo.Repository
+{
+    public class MaterialRepository : IMaterialRepository
+    {
+        private DapperTool dapperTool;
+
+        public MaterialRepository()
+        {
+            this.dapperTool = new DapperTool();
+        }
+
+        public void Create(Material instance)
+        {
+            var sql = @"INSERT INTO [dbo].[Material]
+                        ([Name]
+                        ,[No]
+                        ,[Creater]
+                        ,[LastEditor])
+                         VALUES
+                        (@Name,@No,@Creater,@LastEditor)";
+            var pms = new 
+            {
+                Name = this.dapperTool.ToNVarchar(instance.Name, 250),
+                No = this.dapperTool.ToVarchar(instance.No, 250),
+                Creater = instance.Creater,
+                LastEditor = instance.LastEditor,
+            };
+            var data = this.dapperTool.DapperNonQuery(sql,pms);
+        }
+
+        public void Delete(Material instance)
+        {
+            var sql = @"DELETE FROM [dbo].[Material]
+                        WHERE [ID] = @ID";
+            var pms = new { ID = instance.ID};
+            var data = this.dapperTool.DapperNonQuery(sql,pms);
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Material Get(int primaryID)
+        {
+            var sql = @"SELECT [ID]
+                                ,[Name]
+                                ,[No]
+                                ,[Creater]
+                                ,[CreatDate]
+                                ,[LastEditor]
+                                ,[LastUpdate]
+                            FROM [dbo].[Material]
+                            WHERE [ID] = @ID";
+            var pms = new 
+            {
+                ID = primaryID
+            };
+            var data = this.dapperTool.DapperQuery<Material>(sql, pms).FirstOrDefault();
+            return data;
+        }
+
+        public IList<Material> GetAll()
+        {
+            var sql = @"SELECT [ID]
+                        ,[Name]
+                        ,[No]
+                        ,[Creater]
+                        ,[CreatDate]
+                        ,[LastEditor]
+                        ,[LastUpdate]
+                    FROM [dbo].[Material]";
+            var data = this.dapperTool.DapperQuery<Material>(sql,null);
+            return data;
+        }
+
+        public void Update(Material instance)
+        {
+            var sql = @"UPDATE [dbo].[Material]
+                          SET [Name] = @Name
+                             ,[No] = @No
+                             ,[LastUpdate] = GETDATE()
+                             ,[LastEditor] = @LastEditor
+                        WHERE [ID] = @ID";
+            var pms = new 
+            {
+                Name = this.dapperTool.ToNVarchar(instance.Name,250),
+                No = this.dapperTool.ToVarchar(instance.No,250),
+                LastEditor = instance.LastEditor,
+                ID = instance.ID
+            };
+            var data = this.dapperTool.DapperNonQuery(sql, pms);
+        }
+    }
+}
