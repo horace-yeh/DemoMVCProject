a6f33b0c3f805d78fc134c914a678e202f4c11e6
horace.yeh <horaceyeh.web@gmail.com>
2020-07-31 23:55:12 +0800

    1.實作UserService登入驗證

---
+++ DemoWeb/Demo.Service/UserService.cs
@@ -0,0 +1,32 @@
+﻿using Demo.Data.Models;
+using Demo.Repository;
+using Demo.Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Service
+{
+    public class UserService : IUserService
+    {
+        private UserRepository userRepository;
+
+        public UserService()
+        {
+            this.userRepository = new UserRepository();
+        }
+
+        public bool CheckPassword(User instance)
+        {
+            var user = this.userRepository.GetAll()
+                .Where( x => x.Account.Equals(instance.Account) 
+                        && x.Password.Equals(instance.Password)
+                      );
+            if (user != null)
+                return true;
+            return false;
+        }
+    }
+}
