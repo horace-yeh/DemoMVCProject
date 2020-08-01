54990352425e2ddfd17caade433b57691d59eea5
horace.yeh <horaceyeh.web@gmail.com>
2020-08-01 23:42:28 +0800

    1.調整物料servcie庫存判斷改為庫存service功能 2.調整庫存service介面 3.庫存service實作

---
+++ DemoWeb/Demo.Service/InventoryService.cs
@@ -0,0 +1,55 @@
+﻿using Demo.Data.Models;
+using Demo.Repository;
+using Demo.Repository.Interface;
+using Demo.Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.Service
+{
+    public class InventoryService : IInventoryService
+    {
+        private IInventoryRepository inventoryRepository;
+
+        public InventoryService()
+        {
+            this.inventoryRepository = new InventoryRepository();
+        }
+
+        public IList<Inventory> GetAll()
+        {
+            return this.inventoryRepository.GetAll();
+        }
+
+        public bool HaveInventory(int MaterialID)
+        {
+            return this.inventoryRepository.GetAll().Any(x => x.MaterialID.Equals(MaterialID));
+        }
+
+        public void StockIn(Inventory instance)
+        {
+            if (HaveInventory(instance.MaterialID))
+            {
+                var original = this.inventoryRepository.GetAll().Where(x => x.MaterialID.Equals(instance.MaterialID)).FirstOrDefault();
+                original.Quantity += instance.Quantity;
+                this.inventoryRepository.Update(original);
+                return;
+            }
+            this.inventoryRepository.Create(instance);
+        }
+
+        public void StockOut(Inventory instance)
+        {
+            var original = this.inventoryRepository.GetAll().Where(x => x.MaterialID.Equals(instance.MaterialID)).FirstOrDefault();
+            if (instance.Quantity > original.Quantity)
+            {
+                throw new Exception(@"出庫數量大於庫存數量");
+            }
+            original.Quantity -= instance.Quantity;
+            this.inventoryRepository.Update(original);
+        }
+    }
+}
